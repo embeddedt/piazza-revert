923290376e72a8164069faec7f6df50d81a51b0f
embeddedt <42941056+embeddedt@users.noreply.github.com>
2025-09-28 00:30:54 +0000

    Deploying to main from @ embeddedt/piazza-revert@23fb609e8d5686a4800b8287ff5e272bf8b65baa 🚀

---
+++ revert.meta.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name Piazza Revert
+// @description Revert Piazza to the old user interface
+// @version 0.5.0
+// @author embeddedt
+// @homepage https://github.com/embeddedt/piazza-revert
+// @supportURL https://github.com/embeddedt/piazza-revert/issues
+// @match https://piazza.com/*
+// @downloadURL https://github.com/embeddedt/piazza-revert/raw/refs/heads/main/revert.user.js
+// @grant GM_addStyle
+// @grant GM_addElement
+// @icon https://www.google.com/s2/favicons?sz=64&domain=piazza.com
+// @namespace https://github.com/embeddedt
+// @updateURL https://github.com/embeddedt/piazza-revert/raw/refs/heads/main/revert.user.js
+// ==/UserScript==
