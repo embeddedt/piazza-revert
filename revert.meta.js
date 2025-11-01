8804eaf4897dacf757cc2494c66f9d653bf738b4
embeddedt <42941056+embeddedt@users.noreply.github.com>
2025-11-01 17:27:36 +0000

    Deploying to main from @ embeddedt/piazza-revert@62a0db72f10ea8b730714945444995762ad2c0a4 🚀

--- revert.meta.js
+++ revert.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name Piazza Revert
 // @description Revert Piazza to the old user interface
-// @version 0.5.0-build.2025-11-01T02:39:25.187Z
+// @version 0.5.0-build.2025-11-01T17:27:33.297Z
 // @author embeddedt
 // @homepage https://github.com/embeddedt/piazza-revert
 // @supportURL https://github.com/embeddedt/piazza-revert/issues
